7f1fd914ca54373f903d276894193a489648a89f
Filipp105 <filipp1050@gmail.com>
2024-01-14 18:27:27 +0200

    Отправка файлов Program.cs

--- helloapp/helloapp/Program.cs
+++ helloapp/helloapp/Program.cs
@@ -1,18 +1,6 @@
 var builder = WebApplication.CreateBuilder();
 var app = builder.Build();
 
-app.Run(async (context) =>
-{
-    var path = context.Request.Path;
-    var now = DateTime.Now;
-    var response = context.Response;
-
-    if (path == "/date")
-        await response.WriteAsync($"Date: {now.ToShortDateString()}");
-    else if (path == "/time")
-        await response.WriteAsync($"Time: {now.ToShortTimeString()}");
-    else
-        await response.WriteAsync("Hello METANIT.COM");
-});
+app.Run(async (context) => await context.Response.SendFileAsync("C:\\Users\\Admin\\Desktop\\.net new\\myproject\\helloapp\\helloapp\\forest.jpg"));
 
 app.Run();
