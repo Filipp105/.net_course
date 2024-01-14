a551b33ab026e92a756802c5ea9ca60e86ff8a8f
Filipp105 <filipp1050@gmail.com>
2024-01-14 17:46:58 +0200

    .

--- helloapp/helloapp/Program.cs
+++ helloapp/helloapp/Program.cs
@@ -1,9 +1,4 @@
-WebApplicationBuilder builder = WebApplication.CreateBuilder();
-
-WebApplication app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
-await app.StartAsync();
-await Task.Delay(10000);
-await app.StopAsync();  // через 10 секунд завершаем выполнение приложения
+var builder = WebApplication.CreateBuilder();
+var app = builder.Build();
+app.UseWelcomePage();   // подключение WelcomePageMiddleware
+app.Run();
